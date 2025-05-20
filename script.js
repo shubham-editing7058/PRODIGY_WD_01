03d615d4607c9ce5ddd081ec65555ffe564c8f18
Shubham Gund <shubhamgund183@gmail.com>
2025-05-20 09:38:23 +0530

    Added images folder

---
+++ script.js
@@ -0,0 +1,21 @@
+document.querySelectorAll('.menu a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+document.querySelector('form').addEventListener('submit', e => {
+  e.preventDefault();
+  alert('Thank you for contacting Discover Bali!');
+  e.target.reset();
+});
+window.addEventListener('scroll', () => {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
